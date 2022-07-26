a349f985cc313f8a63584c6e68dc59bd21a59cb8
fayzullo2006 <110022607+fayzullo2006@users.noreply.github.com>
2022-07-26 05:50:21 -0400

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+int n = 65
+        ;
+        if (n % 2 == 0) { // 2ga bo'lib qoldig'ini tekshirish
+            System.out.println("juft");
+        }
+
+        if (n % 2 != 0) { // 2ga bo'lganda qoldig' 0 ga teng bo'lmasa
+            System.out.println("toq");
+        }
+
+    }
+}
